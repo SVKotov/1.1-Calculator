3bcdff34fa3a7ffccb8e528e2c8a73dab5021a0e
SVKotov <100232751+SVKotov@users.noreply.github.com>
2022-04-28 21:58:48 +0300

    Переименованы папки

---
+++ src/Calculator/Main.java
@@ -0,0 +1,18 @@
+package Calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(2, 1);
+        int c = calc.devide.apply(a, b);
+
+        calc.println.accept(a);
+        calc.println.accept(b);
+        calc.println.accept(c);
+
+    }
+}
